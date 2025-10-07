32d3f5e701e18b159fe0ff6a2727d20ecdce5acf
DasFlash <danigku@gmail.com>
2025-10-07 17:19:10 +0200

    exercise2

---
+++ ejercicio2.js
@@ -0,0 +1,21 @@
+console.log(a);
+var a = "hola"; // undefined
+
+//console.log(b);
+//let b = "hola"; // la variable no ha sido inicializada
+
+//console.log(c);
+//const c = "hola"; // la variable no ha sido inicializada
+
+
+sayHi();
+
+function sayHi() {
+  console.log("Hola desde sayHi!"); //Hola desde sayHi!
+}
+
+function sayBye() {
+  console.log("Adios desde sayBye!");
+}
+
+sayBye();
